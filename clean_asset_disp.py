6e3a77957a87551de7496e1fa7dad93181a7a09a
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-02-16 19:31:22 +0100

    Slight changes

--- clean_asset_disp.py
+++ clean_asset_disp.py
@@ -2,20 +2,13 @@ import pandas as pd
 import csv
 from datetime import datetime
 
-df = pd.read_csv('csv_files/asset_disp.csv')
+df = pd.read_csv('csv_files/dispenses.csv')
 
-df = df.drop(columns=['asset_longname', 'block_index', 'tx_hash', 'dispenser'])
+# df = df.drop(columns=['asset_longname', 'block_index', 'tx_hash', 'dispenser'])
 
-df = df.sort_values(by=['timestamp'], ascending=False)
+df.columns = ['address', 'asset_name', 'btc_amount', 'amount', 'date', 'block_id']
 
-# Convert the timestamp column from Unix timestamp to a human-readable date
-df['date'] = df['timestamp'].apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
-df = df.drop(columns=['timestamp'])
-# y = df['date']
-
-# df['date'] = datetime.utcfromtimestamp(y).strftime('%Y-%m-%dT%H:%M:%SZ')
-
-# df = df.sort_values(by=['btc_amount'], ascending=False)
+df = df.sort_values(by=['date'], ascending=False)
 
 # print(df.head(10))
 df.to_csv('csv_files/address_dispenses_by_time.csv')
