b5cb4234e8607511b46bda73bf2dffac4ab0ffb7
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-03-10 14:55:49 +0100

    Modified scripts, updated paths, updated .gitignore

--- scripts/xchain_darkfarms1.sh
+++ scripts/xchain_darkfarms1.sh
@@ -4,11 +4,14 @@
 # change the flag "second_input" to 1 after first run of the script
 # change the path to project's folder for the script to work 
 
+# setting a variable to print input prompts only when running not through console
+export RUNNING_THROUGH_BASH_SCRIPT=1
+
 cd ~/Desktop/Programming/PROGRAMMING_PRACTICE/XCHAIN_BOT
 
 # Set the fixed values for the input
 address="1DRZVQe58Tr9WxDNYdJUbye3toH1zkedX"
-initialized_flag="0"
+initialized_flag="1"
 
 echo "$address" > input.txt
 echo "$initialized_flag" >> input.txt
@@ -17,4 +20,6 @@ python main.py < input.txt
 
 cd csv_files
 column -s, -t < asset_dispenses_1DRZVQe58Tr9WxDNYdJUbye3toH1zkedX.csv | less -N -S
+
+export RUNNING_THROUGH_BASH_SCRIPT=0
 
--- scripts/xchain_insert_address.sh
+++ scripts/xchain_insert_address.sh
@@ -11,9 +11,13 @@ echo $address > input.txt
 read -p "Press '1' if already scraped this address or '0' for first time scraping: " initialized_flag
 echo $initialized_flag >> input.txt
 
+export RUNNING_THROUGH_BASH_SCRIPT=1
+
 python main.py < input.txt
 
 cd csv_files
 csv_file="asset_dispenses_${address}.csv"
 column -s, -t < "$csv_file" | less -N -S
+
+export RUNNING_THROUGH_BASH_SCRIPT=0
 
