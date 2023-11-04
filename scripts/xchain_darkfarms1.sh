a55e83d34e2c35567901453400ef461a5a32ee79
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-11-04 12:32:16 +0100

    Updated bash scripts to create dir and file if needed

--- scripts/xchain_darkfarms1.sh
+++ scripts/xchain_darkfarms1.sh
@@ -6,27 +6,43 @@
 # setting a variable to print input prompts only when running not through console
 export RUNNING_THROUGH_BASH_SCRIPT=1
 
+# Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $SCRIPT_DIR
+# Change to the script's directory
+cd "$SCRIPT_DIR"
+
+# Go up one directory from script location
 cd ..
+
+# Create a directory named 'csv_files' if it doesn't exist
+mkdir -p csv_files
 
 # Set the fixed values for the input
 address="1DRZVQe58Tr9WxDNYdJUbye3toH1zkedX"
 initialized_flag="1"
 
+# Write values to input_from_script.txt
 echo "$address" > input_from_script.txt
 echo "$initialized_flag" >> input_from_script.txt
 
-echo Scraping address $address
-echo With the the initialized flag equal to $initialized_flag. 
-echo Please wait: 
+# Inform user of the process
+echo "Scraping address $address"
+echo "With the initialized flag equal to $initialized_flag." 
+echo "Please wait:" 
 echo
 
+# Run the python script with input redirection
 python3 main.py < input_from_script.txt
 
+# Navigate to the 'csv_files' directory
 cd csv_files
-column -s, -t < asset_dispenses_1DRZVQe58Tr9WxDNYdJUbye3toH1zkedX.csv | less -N -S
 
+# Create the csv file if it doesn't exist
+touch "asset_dispenses_${address}.csv"
+
+# Output the contents of the csv file in a formatted table view
+column -s, -t < "asset_dispenses_${address}.csv" | less -N -S
+
+# Reset the variable after running the script
 export RUNNING_THROUGH_BASH_SCRIPT=0
-
--- scripts/xchain_insert_address.sh
+++ scripts/xchain_insert_address.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-# bash script that runs the python program and later displays the 
+# bash script that runs the python program and later displays the
 # csv file of the scraped address with a column command
 
+# Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $SCRIPT_DIR
+# Navigate to the script's directory
+cd "$SCRIPT_DIR"
 cd ..
 
+# Create the csv_files directory if it doesn't exist
+mkdir -p csv_files
+
+# Prompt user for the address and initialization flag
 read -p "Enter address to scrape: " address
-echo $address > input_from_script.txt
+echo "$address" > input_from_script.txt
 
 read -p "Press '1' if already scraped this address or '0' for first time scraping: " initialized_flag
-echo $initialized_flag >> input_from_script.txt
+echo "$initialized_flag" >> input_from_script.txt
 
+# Set the RUNNING_THROUGH_BASH_SCRIPT variable to ensure correct execution flow
 export RUNNING_THROUGH_BASH_SCRIPT=1
 
+# Run the Python script with input redirection from file
 python3 main.py < input_from_script.txt
 
+# Navigate to the 'csv_files' directory
 cd csv_files
+
+# Create the csv file with the specified address if it doesn't exist
 csv_file="asset_dispenses_${address}.csv"
+touch "$csv_file" # This will create the file if it does not exist
+
+# Display the CSV file content in a tabulated format
 column -s, -t < "$csv_file" | less -N -S
 
+# Reset the RUNNING_THROUGH_BASH_SCRIPT variable
 export RUNNING_THROUGH_BASH_SCRIPT=0
-
