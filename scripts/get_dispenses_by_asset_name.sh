e0b5d3667ef57cf54856534737bc92e032f03a69
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-11-25 23:30:41 +0100

    Added a bash script to grep dispenses by asset name

---
+++ scripts/get_dispenses_by_asset_name.sh
@@ -0,0 +1,7 @@
+#!/opt/homebrew/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+read -p "What asset dispenses would you like to see? `echo $'\n> '`" asset_name
+
+grep ${asset_name} "${SCRIPT_DIR}/../dispenses.txt"
