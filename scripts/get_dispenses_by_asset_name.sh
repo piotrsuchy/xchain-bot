8736327ab8f728bcafe73961c1cd21a2d4f0cdbd
Piotr Suchy <piotrsuchy6@tlen.pl>
2024-01-30 23:54:40 +0100

    Update to get_dispenses_by_asset_name script

--- scripts/get_dispenses_by_asset_name.sh
+++ scripts/get_dispenses_by_asset_name.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 read -p "What asset dispenses would you like to see? `echo $'\n> '`" asset_name
 
+echo "Grepping through dispenses.txt for ${asset_name}"
 grep ${asset_name} "${SCRIPT_DIR}/../dispenses.txt"
